0855b6b34e91c55d9781792b9546e0d3481cd2fa
ChuaCL <chuacheelong.332@gmail.com>
2024-07-21 19:39:12 +0800

    first commit

---
+++ js/set_url.js
@@ -0,0 +1,6 @@
+//Set URL Path
+let web_parent = window.location.origin;
+        
+//set the image path for logo
+let logo_image = web_parent+'/resource/images/ResumeLogo.png';
+let main_page_url = web_parent+'/html/home_page.html';
